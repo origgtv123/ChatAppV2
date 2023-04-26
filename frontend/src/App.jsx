11a5a292b3224d10730920e0e4eb47de57c01d23
origgtv123 <106916777+origgtv123@users.noreply.github.com>
2023-04-26 18:14:22 +0300

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import { useState } from 'react';
 import AuthPage from './AuthPage.jsx';
 import ChatsPage from './ChatsPage.jsx';
-
-
-
-import "./App.css";
+import "./App.css"
 
 
 function App() {
   const [user, setUser] = useState(undefined);
 
+  const onAuth = (user) => {
+    setUser(user);
+  };
+
   if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
+    return <AuthPage onAuth={onAuth} />;
   } else {
     return <ChatsPage user={user} />;
   }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
